60b9dc8bdd9667f43369cd7bc274a749e5487987
Stradivarius <bqiang3.14@gmail.com>
2026-09-20 22:30:14 -0400

    fix race condition?

--- src/main/java/events/TicketCreate.java
+++ src/main/java/events/TicketCreate.java
@@ -4,19 +4,20 @@ import main.Main;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class TicketCreate extends ListenerAdapter {
-	public void onChannelCreate(ChannelCreateEvent e) {
-		if(e.getChannel() instanceof ThreadChannel channel) {
-			if(Utils.isForumChannel(channel)) {
-				Utils.editPost(channel, "", "open", "");
+	public void onMessageReceived(MessageReceivedEvent e) {
+		if(e.getChannel() instanceof ThreadChannel thread && Utils.isForumChannel(thread) && e.getMessageId().equals(thread.getId())) {
+			if(Utils.isForumChannel(thread)) {
+				Utils.editPost(thread, "", "open", "");
 				String message = """
 				**OPEN TICKET**
 				TAs: To claim this post, run `!claim` in this channel.
 				OP: To close this post, run `!close` in this channel.
 				""";
-				Message sentMessage = channel.sendMessage(message).complete();
+				Message sentMessage = thread.sendMessage(message).complete();
 				sentMessage.pin().queue();
 			}
 		}
